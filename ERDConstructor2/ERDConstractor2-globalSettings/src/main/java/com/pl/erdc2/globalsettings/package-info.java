9ba41535e9a8d42f10c3252180cb51c8bfa39b5b
Piotr Rutkowski <piorutko@gmail.com>
2015-08-12 23:02:22 +0200

    Globalne ustawienia projektu ERDC-41
    * Dodanie modułu ustawień

---
+++ ERDConstructor2/ERDConstractor2-globalSettings/src/main/java/com/pl/erdc2/globalsettings/package-info.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@OptionsPanelController.ContainerRegistration(
+        id = "ERDConstractor2Settings", 
+        categoryName = "#OptionsCategory_Name_ERDConstractor2Settings", 
+        iconBase = "com/pl/erdc2/globalsettings/gear-icon.png", 
+        keywords = "#OptionsCategory_Keywords_ERDConstractor2Settings", 
+        keywordsCategory = "ERDConstractor2Settings")
+@NbBundle.Messages(
+        value = {
+            "OptionsCategory_Name_ERDConstractor2Settings=ERD Constractor 2 Settings", 
+            "OptionsCategory_Keywords_ERDConstractor2Settings=settings erd"})
+package com.pl.erdc2.globalsettings;
+
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.NbBundle;
