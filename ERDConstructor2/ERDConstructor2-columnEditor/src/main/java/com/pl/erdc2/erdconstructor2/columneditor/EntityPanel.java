28958fadf891084d73272491fe00746cae5cdcbb
Piotr Czarnecki <piotrek.reda@gmail.com>
2015-09-14 23:06:55 +0200

    fix po złym mergu

--- ERDConstructor2/ERDConstructor2-columnEditor/src/main/java/com/pl/erdc2/erdconstructor2/columneditor/EntityPanel.java
+++ ERDConstructor2/ERDConstructor2-columnEditor/src/main/java/com/pl/erdc2/erdconstructor2/columneditor/EntityPanel.java
@@ -2,6 +2,7 @@ package com.pl.erdc2.erdconstructor2.columneditor;
 
 import com.pl.erdc2.erdconstructor2.api.Column;
 import com.pl.erdc2.erdconstructor2.api.ColumnNode;
+import com.pl.erdc2.erdconstructor2.api.Entity;
 import com.pl.erdc2.erdconstructor2.api.EntityNode;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -17,10 +18,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import org.openide.nodes.Node;
 import org.openide.util.Exceptions;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -30,14 +34,15 @@ public class EntityPanel extends JPanel{
     JTable table;
     JLabel entityLablel;
     JLabel nameLablel;
-    JLabel nameField;
+    JLabel descriptionLabel;
+    JTextField nameField;
+    JTextArea descriptionField;
     JScrollPane tablePanel;
     JButton addButton;
     EntityNode selectedNode = null;
     private static final Logger logger = Logger.getLogger(EntityPanel.class);
 
     public EntityPanel() {
-        BasicConfigurator.configure();
         setName(Bundle.CTL_ColumnEditorTopComponent());
         setToolTipText(Bundle.HINT_ColumnEditorTopComponent());
                 
@@ -60,8 +65,40 @@ public class EntityPanel extends JPanel{
         entityLablel.setFont(new Font("Calibri", Font.PLAIN, 24));
         nameLablel = new JLabel(Bundle.Name());
         nameLablel.setFont(new Font("Calibri", Font.BOLD, 17));
-        nameField = new JLabel();
-        nameField.setFont(new Font("Calibri", Font.PLAIN, 17));
+        nameField = new JTextField();
+        descriptionLabel = new JLabel(Bundle.Description());
+        descriptionLabel.setFont(new Font("Calibri", Font.BOLD, 17));
+        descriptionField = new JTextArea();
+        nameField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                changeName();
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                changeName();
+            }
+            @Override
+            public void changedUpdate(DocumentEvent e){
+                changeName();
+            }
+        });
+        descriptionField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                changeDesc();
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                changeDesc();
+            }
+            @Override
+            public void changedUpdate(DocumentEvent e){
+                changeDesc();
+            }
+        });
+        
+        
         
         addButton = new JButton();
         addButton.setEnabled(false);
@@ -84,17 +121,44 @@ public class EntityPanel extends JPanel{
         add(entityLablel, gbc);
         gbc.gridy = 1;
         add(nameLablel, gbc);
-        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         add(nameField, gbc);
         gbc.gridx = 0;
-        gbc.gridy = 2;
-        add(addButton, gbc);
         gbc.gridy = 3;
+        add(descriptionLabel, gbc);
+        gbc.gridy = 4; 
+        gbc.insets = new Insets(5,15,20,15);
+        gbc.fill = GridBagConstraints.BOTH;
+        add(descriptionField, gbc);
+        gbc.insets = new Insets(5,15,0,15);
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridy = 5;
+        add(addButton, gbc);
+        gbc.gridy = 6;
         gbc.gridwidth=2;
         gbc.gridheight=13;
         gbc.weighty=10;
         gbc.fill = GridBagConstraints.BOTH;
         add(tablePanel, gbc);
+    }
+    
+    private void changeName(){
+        if(selectedNode==null)
+            return;
+        Entity  en = selectedNode.getLookup().lookup(Entity.class);
+        if(en==null)
+            return;
+        en.setName(nameField.getText());
+    }
+    
+    private void changeDesc(){
+        if(selectedNode==null)
+            return;
+        Entity en = selectedNode.getLookup().lookup(Entity.class);
+        if(en==null)
+            return;
+        en.setDescription(descriptionField.getText());
     }
     
     private void addNewColumn(){
@@ -128,6 +192,7 @@ public class EntityPanel extends JPanel{
         
         addButton.setEnabled(true);
         nameField.setText(selectedNode.getDisplayName());
+        descriptionField.setText(selectedNode.getLookup().lookup(Entity.class).getDescription());
         model.fireTableDataChanged();
     }
 }
