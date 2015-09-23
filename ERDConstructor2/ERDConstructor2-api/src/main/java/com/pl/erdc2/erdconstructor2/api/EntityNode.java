0acaf6f2e030215a383a627e9827a945c0e01985
Kubiuss <kubiuss@o2.pl>
2015-09-23 19:23:53 +0200

    no message

--- ERDConstructor2/ERDConstructor2-api/src/main/java/com/pl/erdc2/erdconstructor2/api/EntityNode.java
+++ ERDConstructor2/ERDConstructor2-api/src/main/java/com/pl/erdc2/erdconstructor2/api/EntityNode.java
@@ -14,7 +14,7 @@ import org.openide.util.lookup.Lookups;
     "# {0} - entity",
     "EntityDefaultName=Entity {0}"
 })
-public class EntityNode extends BeanNode<Entity> {  
+public class EntityNode extends BeanNode<Entity> implements Observer {  
     public EntityNode(Entity bean) throws IntrospectionException {
         super(bean, Children.LEAF, Lookups.singleton(bean));
         if(bean.getId()==0){
@@ -52,4 +52,18 @@ public class EntityNode extends BeanNode<Entity> {
         }
         return ++max;
     }
+    
+    @Override
+     public void update(Observable o, Object arg) {
+         if(!(o instanceof Entity))
+             return;
+         
+         Entity col = (Entity)o;
+         String property = (String)arg;
+         if(property.equals("name")){ 
+             String old = this.getDisplayName();
+             this.setDisplayName(col.getName());
+             this.fireDisplayNameChange(old, col.getName());
+         }
+     }
 }
