531cc3219396bf595b5c1e671fcf97b2d4c42c8d
Piotr Czarnecki <piotrek.reda@gmail.com>
2015-10-05 20:32:54 +0200

    nawiasy

--- ERDConstructor2/ERDConstructor2-columnEditor/src/main/java/com/pl/erdc2/erdconstructor2/columneditor/ColumnNodeListener.java
+++ ERDConstructor2/ERDConstructor2-columnEditor/src/main/java/com/pl/erdc2/erdconstructor2/columneditor/ColumnNodeListener.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pl.erdc2.erdconstructor2.columneditor;
 
 import org.openide.nodes.NodeAdapter;
@@ -14,16 +9,12 @@ import org.openide.nodes.NodeEvent;
  */
 public class ColumnNodeListener extends NodeAdapter{
     private EntityPanel ep;
-    public ColumnNodeListener(EntityPanel ep)
-    {
+    public ColumnNodeListener(EntityPanel ep){
         this.ep=ep;
     }
     
     @Override
-    public void nodeDestroyed (NodeEvent ev)
-    {
+    public void nodeDestroyed (NodeEvent ev){
         ep.updateTable();
     }
-    
-    
 }
