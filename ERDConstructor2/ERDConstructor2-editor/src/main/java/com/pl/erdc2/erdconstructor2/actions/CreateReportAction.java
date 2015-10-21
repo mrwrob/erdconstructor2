dd3c0a5ca29bf61a39a00497e48b5076cbe57fbb
Piotr Czarnecki <piotrek.reda@gmail.com>
2015-10-21 16:03:44 +0200

    ERDC-80 tabelki do raportu

--- ERDConstructor2/ERDConstructor2-editor/src/main/java/com/pl/erdc2/erdconstructor2/actions/CreateReportAction.java
+++ ERDConstructor2/ERDConstructor2-editor/src/main/java/com/pl/erdc2/erdconstructor2/actions/CreateReportAction.java
@@ -16,7 +16,6 @@ import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileFilter;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.apache.poi.xwpf.usermodel.Borders;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.TextAlignment;
 import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
@@ -38,7 +37,16 @@ import com.pl.erdc2.globalsettings.UserInfoPanel;
 import helper.CustomXWPFDocument;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTbl;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblWidth;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTcPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STTblWidth;
 /**
  *
  * @author Piotr
@@ -47,7 +55,12 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 @ActionRegistration(displayName = "#CTL_CreateReportAction")
 @ActionReference(path = "Menu/File", position = 14)
 @NbBundle.Messages({"CTL_CreateReportAction=Create report",
-        "NameAndSurname=Imię i nazwisko"})
+        "NameAndSurname=Imię i nazwisko",
+        "Confirm_Replace_File=Czy na pewno chcesz nadpisać plik?",
+        "Confirm=Potwierdzenie",
+        "Yes_Option=Tak",
+        "No_Option=Nie"})
+
 public class CreateReportAction implements ActionListener{
     
     private static final Logger logger = Logger.getLogger(CreateReportAction.class);
@@ -69,42 +82,44 @@ public class CreateReportAction implements ActionListener{
             
             XWPFRun r1 = p1.createRun();
             r1.setBold(true);
-            r1.setFontSize(16);
+            r1.setFontSize(14);
             String name = userSettings.getFirstName() + " " + userSettings.getLastName();
             r1.setText(name.trim().equals("") ? Bundle.NameAndSurname() : name);
             r1.setFontFamily("Calibri");
             r1.addCarriageReturn();
             r1.setBold(true);
             r1.setText("Indeks: " + userSettings.getIndexNo());
-
+            r1.addBreak();
+            
             XWPFParagraph p2 = doc.createParagraph();
             p2.setAlignment(ParagraphAlignment.LEFT);
 
             XWPFRun r2 = p2.createRun();
-            r2.setFontSize(20);
+            r2.setFontSize(17);
             r2.setBold(true);
             r2.setUnderline(UnderlinePatterns.WORDS);
-            r2.setText("Temat projektu:");
-
-            XWPFRun r3 = p2.createRun();
-            r3.setFontSize(12);
-            r3.setText("Temat twojego projektu ...");
+            r2.setFontFamily("Cambria");
+            r2.setText("Temat projektu");
+            r2.addCarriageReturn();
             r2.addCarriageReturn();
             
             XWPFParagraph p3 = doc.createParagraph();
             p3.setAlignment(ParagraphAlignment.LEFT);
             
             XWPFRun r4 = p3.createRun();
-            r4.setFontSize(20);
+            r4.setFontSize(17);
             r4.setBold(true);
             r4.setUnderline(UnderlinePatterns.WORDS);
-            r4.setText("Szczegółowy opis projektu:");
+            r4.setFontFamily("Cambria");
+            r4.setText("Szczegółowy opis projektu");
+            r4.addCarriageReturn();
             r4.addCarriageReturn();
 
             XWPFRun r5 = p3.createRun();
             r5.setFontSize(12);
             r5.setText("Szczegółowy opis twojego projektu ...");
-            r2.addCarriageReturn();
+            r5.addCarriageReturn();
+            r5.setFontFamily("Calibri");
 
             XWPFParagraph p4 = doc.createParagraph();
             p4.setPageBreak(true);
@@ -113,7 +128,9 @@ public class CreateReportAction implements ActionListener{
             r6.setFontSize(20);
             r6.setBold(true);
             r6.setUnderline(UnderlinePatterns.WORDS);
-            r6.setText("Diagram ERD:");           
+            r6.setFontFamily("Cambria");
+            r6.setText("Diagram ERD");      
+            r6.addBreak();
 
             EditorTopComponent etc;
             etc = (EditorTopComponent) WindowManager.getDefault().findTopComponent("editorTopComponent");
@@ -138,45 +155,107 @@ public class CreateReportAction implements ActionListener{
             r8.setFontSize(20);
             r8.setBold(true);
             r8.setUnderline(UnderlinePatterns.WORDS);
-            r8.setText("Opis zbioru encji:");
+            r8.setText("Opis zbioru encji");
+            r8.setFontFamily("Cambria");
+            r8.addBreak();
 
             for(Node n : EntityExplorerManagerProvider.getEntityNodeRoot().getChildren().getNodes()){
-                    Entity ent = n.getLookup().lookup(Entity.class);  
-                    
-                    XWPFParagraph p6 = doc.createParagraph();
-                    p6.setPageBreak(false);
-                    p6.setBorderBottom(Borders.DOUBLE);
-                    p6.setBorderTop(Borders.DOUBLE);
-                    p6.setBorderRight(Borders.DOUBLE);
-                    p6.setBorderLeft(Borders.DOUBLE);
-                    p6.setBorderBetween(Borders.SINGLE);
-        
-                    XWPFRun r9 = p6.createRun();
-                    r9.setFontSize(12);
-                    r9.setBold(true);
-                    r9.setText("Zbiór encji: " + (ent.getName()==null ? "" : ent.getName()));
-                    r9.addCarriageReturn();
-                    
-                    XWPFRun r10 = p6.createRun();
-                    r10.setFontSize(12);
-                    r10.setText("Opis: " + (ent.getDescription()==null ? "" : ent.getDescription()));
-                    
-                    for(Node columnNode : n.getChildren().getNodes()){
-                        Column c = columnNode.getLookup().lookup(Column.class);
-                        
-                        XWPFParagraph p7 = doc.createParagraph();
-                        p7.setPageBreak(false);                        
-                        XWPFRun r11 = p7.createRun();
-                        r11.setFontSize(12);
-                        r11.setText("Nazwa kolumny: " + (c.getName()==null ? "" : c.getName()));
-                        r11.addCarriageReturn();
-                        r11.setText("Klucz główny: " + (c.isPrimary() ? "TAK" : "NIE"));
-                        r11.addCarriageReturn();
-                        r11.setText("Typ kolumny: " + (c.getType()==null ? "" : c.getType()));
-                        r11.addCarriageReturn();
-                        r11.setText("Opis kolumny: " + c.getDescription());
-                        r11.addBreak();
-                    }
+                Entity ent = n.getLookup().lookup(Entity.class);  
+                XWPFTable table = doc.createTable();
+                CTTbl tablec        = table.getCTTbl();
+                CTTblPr pr         = tablec.getTblPr();
+                CTTblWidth  tblW = pr.getTblW();
+                tblW.setW(BigInteger.valueOf(5000));
+                tblW.setType(STTblWidth.PCT);
+                pr.setTblW(tblW);
+                tablec.setTblPr(pr);
+
+                XWPFTableRow tableRow1 = table.getRow(0);
+                XWPFParagraph p = tableRow1.getCell(0).getParagraphs().get(0);
+                p.setAlignment(ParagraphAlignment.CENTER);
+                XWPFRun r = p.createRun();
+                r.setFontSize(14);
+                r.setBold(true);
+                r.setText(ent.getName());
+                r.setFontFamily("Calibri");
+                tableRow1.getCell(0).setVerticalAlignment(XWPFTableCell.XWPFVertAlign.CENTER);
+
+
+                XWPFTableRow tableRow2 = table.createRow();
+                p = tableRow2.getCell(0).getParagraphs().get(0);
+                p.setAlignment(ParagraphAlignment.CENTER);
+                r = p.createRun();
+                r.setFontFamily("Calibri");
+                r.setText((ent.getDescription()==null ? "Opis encji" : ent.getDescription()));
+
+
+                XWPFTableRow tableRow3 = table.createRow();
+                p = tableRow3.getCell(0).getParagraphs().get(0);
+                p.setAlignment(ParagraphAlignment.CENTER);
+                r = p.createRun();
+                r.setBold(true);
+                r.setFontFamily("Calibri");
+                r.setText("Nazwa");
+
+                p = tableRow3.addNewTableCell().getParagraphs().get(0);
+                p.setAlignment(ParagraphAlignment.CENTER);
+                r = p.createRun();
+                r.setBold(true);
+                r.setFontFamily("Calibri");
+                r.setText("Klucz główny");
+
+                p = tableRow3.addNewTableCell().getParagraphs().get(0);
+                p.setAlignment(ParagraphAlignment.CENTER);
+                r = p.createRun();
+                r.setBold(true);
+                r.setFontFamily("Calibri");
+                r.setText("Typ/dziedzina");
+
+                p = tableRow3.addNewTableCell().getParagraphs().get(0);
+                p.setAlignment(ParagraphAlignment.CENTER);
+                r = p.createRun();
+                r.setBold(true);
+                r.setFontFamily("Calibri");
+                r.setText("Opis");
+
+                spanCellsAcrossRow(table, 0, 0, 4);
+                spanCellsAcrossRow(table, 1, 0, 4);
+
+                for(Node columnNode : n.getChildren().getNodes()){
+                    Column c = columnNode.getLookup().lookup(Column.class);
+
+                    XWPFTableRow tableRowA = table.createRow();
+                    p = tableRowA.getCell(0).getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(c.getName()==null ? "" : c.getName());
+
+                    p = tableRowA.addNewTableCell().getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    if(c.isPrimary())
+                        r.setBold(true);
+                    r.setText(c.isPrimary() ? "Tak" : "Nie");
+
+                    p = tableRowA.addNewTableCell().getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(c.getType()==null ? "" : c.getType());
+
+                    p = tableRowA.addNewTableCell().getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(c.getDescription());
+
+                }
+
+                p = doc.createParagraph();
+                p.setPageBreak(false);
+                p.createRun().addBreak();
             } 
 
             XWPFParagraph p8 = doc.createParagraph();
@@ -184,47 +263,113 @@ public class CreateReportAction implements ActionListener{
             r12.setFontSize(20);
             r12.setBold(true);
             r12.setUnderline(UnderlinePatterns.WORDS);
-            r12.setText("Opis związków:");
+            r12.setText("Opis związków");
+            r12.setFontFamily("Cambria");
             r12.addCarriageReturn();
+            
+            XWPFTable table = doc.createTable();
+            CTTbl tablec        = table.getCTTbl();
+            CTTblPr pr         = tablec.getTblPr();
+            CTTblWidth  tblW = pr.getTblW();
+            tblW.setW(BigInteger.valueOf(5000));
+            tblW.setType(STTblWidth.PCT);
+            pr.setTblW(tblW);
+            tablec.setTblPr(pr);
+
+            XWPFTableRow tableRow = table.createRow();
+            XWPFParagraph p;
+            XWPFRun r;
+            p = tableRow.getCell(0).getParagraphs().get(0);
+            p.setAlignment(ParagraphAlignment.CENTER);
+            r = p.createRun();
+            r.setBold(true);
+            r.setFontFamily("Calibri");
+            r.setText("Nazwa związku");
+
+            p = tableRow.addNewTableCell().getParagraphs().get(0);
+            p.setAlignment(ParagraphAlignment.CENTER);
+            r = p.createRun();
+            r.setBold(true);
+            r.setFontFamily("Calibri");
+            r.setText("Zbiór encji 1");
+
+            p = tableRow.addNewTableCell().getParagraphs().get(0);
+            p.setAlignment(ParagraphAlignment.CENTER);
+            r = p.createRun();
+            r.setBold(true);
+            r.setFontFamily("Calibri");
+            r.setText("Zbiór encji 2");
+
+            p = tableRow.addNewTableCell().getParagraphs().get(0);
+            p.setAlignment(ParagraphAlignment.CENTER);
+            r = p.createRun();
+            r.setBold(true);
+            r.setFontFamily("Calibri");
+            r.setText("Liczność związku");
+            
+            p = tableRow.addNewTableCell().getParagraphs().get(0);
+            p.setAlignment(ParagraphAlignment.CENTER);
+            r = p.createRun();
+            r.setBold(true);
+            r.setFontFamily("Calibri");
+            r.setText("Opis");
             
             for(Node n : EntityExplorerManagerProvider.getRelatioshipNodeRoot().getChildren().getNodes()){
                     Relationship rel = n.getLookup().lookup(Relationship.class);
                     
-                    XWPFParagraph p9 = doc.createParagraph();
-                    p9.setPageBreak(false);
-                    XWPFRun r13 = p9.createRun();
-                    r13.setFontSize(12);
-                    r13.setBold(false);
-                    r13.setText("Nazwa związku: " + rel.getName());
-                    r13.addCarriageReturn();
+                    XWPFTableRow tableRowA = table.createRow();
+                    p = tableRowA.getCell(0).getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(rel.getName());
+
                     Entity e1 = EntityExplorerManagerProvider.getEntityById(rel.getSourceEntityId());
-                    if(e1!=null){
-                        r13.setText("Encja 1: " + e1.getName());
-                        r13.addCarriageReturn();
-                    }
+                    p = tableRowA.addNewTableCell().getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(e1.getName());
+
                     Entity e2 = EntityExplorerManagerProvider.getEntityById(rel.getDestinationEntityId());
-                    if(e2!=null){
-                        r13.setText("Encja 2: " + e2.getName());
-                        r13.addCarriageReturn();
-                    }
-                    r13.setText("Typ związku: " + rel.getSourceType()+" : "+rel.getDestinationType());
-                    r13.addCarriageReturn();
-                    r13.setText("Opis związku: " + (rel.getDescription()==null ? "" : rel.getDescription()));
-                    r13.addCarriageReturn();
+                    p = tableRowA.addNewTableCell().getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(e2.getName());
+
+                    p = tableRowA.addNewTableCell().getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(rel.getSourceType()+" : "+rel.getDestinationType());
+                    
+                    p = tableRowA.addNewTableCell().getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.CENTER);
+                    r = p.createRun();
+                    r.setFontFamily("Calibri");
+                    r.setText(rel.getDescription()==null ? "" : rel.getDescription());
             }
+            
+            p = doc.createParagraph();
+            p.setPageBreak(false);
+            p.createRun().addBreak();
             
             XWPFParagraph p10 = doc.createParagraph();
             XWPFRun r14 = p10.createRun();
             r14.setFontSize(20);
             r14.setBold(true);
             r14.setUnderline(UnderlinePatterns.WORDS);
+            r14.setFontFamily("Cambria");
             r14.setText("Schemat relacyjnej bazy danych");
             
                     
             String dictionaryPath = dictionaryPath();
-            FileOutputStream out = new FileOutputStream(dictionaryPath);
-            doc.write(out);
-            out.close();
+            if(dictionaryPath != null){
+                FileOutputStream out = new FileOutputStream(dictionaryPath);            
+                doc.write(out);
+                out.close();
+            }                        
             
         }catch(IOException | InvalidFormatException ex){
             JOptionPane.showMessageDialog(null, ex);
@@ -259,12 +404,39 @@ public class CreateReportAction implements ActionListener{
                 return "Word Document (.docx)";
             }
         });
-        
-        chooser.showSaveDialog(chooser);
-        String fullPath = chooser.getSelectedFile().getAbsolutePath();
-        if(!fullPath.endsWith(".docx")){
-            return fullPath + ".docx";
+                
+        while(true){            
+            int chooserResponse = chooser.showSaveDialog(chooser);            
+            if(chooserResponse == JFileChooser.CANCEL_OPTION){
+                return null;
+            }
+            
+            String fullPath = chooser.getSelectedFile().getAbsolutePath();            
+            if(!fullPath.endsWith(".docx")){
+                return fullPath + ".docx";
+            }
+   
+            File testFile = new File(fullPath);
+            if(testFile.exists()){
+                int response = JOptionPane.showOptionDialog(null,
+                        (Object) Bundle.Confirm_Replace_File(), 
+                        Bundle.Confirm(), JOptionPane.YES_NO_OPTION, 
+                        JOptionPane.INFORMATION_MESSAGE, null, 
+                        new String[]{Bundle.Yes_Option(), Bundle.No_Option()}, "default");
+                if (response == JOptionPane.YES_OPTION) {
+                    System.out.println("aa");
+                    return fullPath;
+                } 
+            }
         }
-        return fullPath;
+    }
+    
+    private void spanCellsAcrossRow(XWPFTable table, int rowNum, int colNum, int span) {
+        XWPFTableCell  cell = table.getRow(rowNum).getCell(colNum);
+        CTTcPr tcPr = cell.getCTTc().getTcPr();
+        if (null == tcPr)    
+            tcPr = cell.getCTTc().addNewTcPr();
+        tcPr.addNewGridSpan();
+        cell.getCTTc().getTcPr().getGridSpan().setVal(BigInteger.valueOf((long)span));
     }
 }
